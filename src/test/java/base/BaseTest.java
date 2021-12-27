3f58c1895a4a6a28e9220d4a0aa5d17c8feaaa5d
timurmalkoc <malkoctimurcelik@gmil.com>
2021-12-27 16:30:36 -0500

    Selenium take screenshot for failing test

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,12 +1,20 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import util.WindowManager;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest {
     private WebDriver driver;
@@ -43,7 +51,21 @@ public class BaseTest {
 
     @AfterClass
     public void tearDown(){
-       // driver.quit();
+        driver.quit();
+    }
+
+    // run for all methods but take shot for fail tests
+    @AfterMethod
+    public void recordFailure(ITestResult result)  {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            TakesScreenshot cam = (TakesScreenshot) driver;
+            File screenshot = cam.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("resources/screenshots/"+ result.getTestName() +".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
